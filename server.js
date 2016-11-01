6547374e814c251715d27230f02835d7b825dd5a
Taylor Emolo <tayloremolo@tay.local>
2016-11-01 09:41:08 -0400

    HTML content created

---
+++ server.js
@@ -0,0 +1,15 @@
+// Dependencies
+// ===========================================================
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
