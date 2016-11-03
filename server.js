fdfb8b0b303611d0d546b98e3ed1d5a72a7cfe69
Taylor Emolo <tayloremolo@tay.local>
2016-11-03 17:53:26 -0400

    server.js adj.

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 // DEPENDENCIES===========================================================
 // NPM Packages needed
-var express 	= require('express');
-var bodyParser  = require('body-parser');
-var path 		= require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
 // =======================================================================
 // Set up express app
 var app  = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3006;
 
 // =======================================================================
 // Set up data parsing for express app 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-app.use(express.static('app'));
+app.use(express.static('app');
 
 // ROUTES =================================================================
 // route files needed to map out the site
@@ -25,6 +25,6 @@ require('./app/routing/html-routes.js')(app);
 
 // LISTENER ===============================================================
 // LISTENER - Starts the server
-app.listen(PORT, function () {
+app.listen(PORT, function(){
     console.log('App listening on PORT ' + PORT);
 });
